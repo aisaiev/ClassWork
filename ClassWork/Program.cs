aaa282b5686f1e46f1a6a3e0e1554ff8eac6c3c5
Anton <antonio.de.tores@gmail.com>
2019-04-18 19:24:22 +0300

    Task4 impelemted

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -29,5 +29,21 @@ namespace ClassWork
         {
             return initialString.Insert(index, stringToInsert);
         }
+
+        private static void Task4(string str)
+        {
+            int questionMarkCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '?')
+                    questionMarkCount++;
+                if (questionMarkCount == 1)
+                    if (str[i] == ' ')
+                        str = str.Remove(i, 1);
+                if (questionMarkCount == 2)
+                    break;
+            }
+            Console.WriteLine(str);
+        }
     }
 }
