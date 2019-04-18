dd4948b43a32089aa62b62fb75636fd1137a6405
Anton <antonio.de.tores@gmail.com>
2019-04-18 18:54:32 +0300

    Task2 implemented

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -10,6 +10,20 @@ namespace ClassWork
     {
         static void Main(string[] args)
         {
+
+        }
+
+        private static int Task2(string lineString, char symbol)
+        {
+            int count = 0;
+            for (int i = 0; i < lineString.Length; i++)
+            {
+                if (lineString[i] == symbol)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
