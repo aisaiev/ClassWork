ba6783a48166c4fb1197b3c2585292eeef4910fd
Anton <antonio.de.tores@gmail.com>
2019-04-20 10:47:18 +0300

    Task1 updated

---
+++ ClassWork/Task1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    public static class Task1
+    {
+        public static void Calculate()
+        {
+            Console.Write("Enter the first number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+            bool isOperationValid = false;
+
+            while (!isOperationValid)
+            {
+                Console.Write("Enter operation [+ - * /]: ");
+                string sign = Console.ReadLine();
+                switch (sign)
+                {
+                    case "+":
+                        Add(firstNumber, secondNumber);
+                        isOperationValid = true;
+                        break;
+                    case "-":
+                        Sub(firstNumber, secondNumber);
+                        isOperationValid = true;
+                        break;
+                    case "*":
+                        Mul(firstNumber, secondNumber);
+                        isOperationValid = true;
+                        break;
+                    case "/":
+                        Div(firstNumber, secondNumber);
+                        isOperationValid = true;
+                        break;
+                    default:
+                        Console.WriteLine("Plese enter valid math operation!");
+                        isOperationValid = false;
+                        break;
+                }
+            }
+        }
+
+        private static void Add(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+        }
+
+        private static void Sub(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+        }
+
+        private static void Mul(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+        }
+
+        private static void Div(double firstNumber, double secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Error: divide by zero operation");
+            }
+        }
+    }
+}
