955c064337503a91963d37f0343e6933ca83f527
Anton <antonio.de.tores@gmail.com>
2019-04-23 20:15:12 +0300

    Task7 implemented

---
+++ ClassWork/Task7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    public static class Task7
+    {
+        public static string ToCamelCase(string str)
+        {
+            string[] words = str.Split('-');
+            if (words.Length == 1)
+            {
+                words = str.Split('_');
+            }
+            string[] resultWords = new string[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i != 0)
+                {
+                    resultWords[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+                }
+                else
+                {
+                    resultWords[i] = words[i];
+                }
+            }
+            return string.Join("", resultWords);
+        }
+    }
+}
