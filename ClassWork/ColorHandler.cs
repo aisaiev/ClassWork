817c1d20f85f4d3b4e0bf43a74a070cefdcfcb56
Anton <antonio.de.tores@gmail.com>
2019-04-28 20:29:17 +0300

    Task3 implemented

---
+++ ClassWork/ColorHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    class ColorHandler
+    {
+        public static void Print(string stroka, int color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(stroka);
+        }
+    }
+
+    public enum Color
+    {
+        White = 15,
+        Red = 12,
+        Green = 10
+    }
+}
--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -11,14 +11,7 @@ namespace ClassWork
     {
         static void Main(string[] args)
         {
-            MyClass myClass = new MyClass();
-            MyStruct myStruct = new MyStruct();
-            myClass.change = "не изменено";
-            myStruct.change = "не изменено";
-            Handler.ClassTaker(myClass);
-            Handler.StructTaker(myStruct);
-            Console.WriteLine($"myClass.change = {myClass.change}");
-            Console.WriteLine($"myStruct.change = {myStruct.change}");
+            ColorHandler.Print("test string", (int)Color.White);
         }
     }
 }
