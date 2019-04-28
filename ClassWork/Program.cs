0001d927739769a6c61fcd707cb056bbf1761e55
Anton <antonio.de.tores@gmail.com>
2019-04-28 19:59:23 +0300

    Tas2 implemented

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,40 @@ namespace ClassWork
     {
         static void Main(string[] args)
         {
+            Train[] trains = FillTrainsArray(2);
+            Train train = FindTrainByTrainNumber(trains);
+            if (train != null)
+            {
+                train.PrintTrainInfo();
+            }
+            else
+            {
+                Console.WriteLine("Train is not found");
+            }
+        }
+
+        private static Train[] FillTrainsArray(int numberOfTrains)
+        {
+            Train[] trains = new Train[numberOfTrains];
+            for (int i = 0; i < numberOfTrains; i++)
+            {
+                Console.Write("Enter train number: ");
+                string trainNumber = Console.ReadLine();
+                Console.Write("Enter destination: ");
+                string destination = Console.ReadLine();
+                Console.Write("Enter departure time (yyyy-mm-dd hh:mm): ");
+                string departureTime = Console.ReadLine();
+                Train train = new Train(trainNumber, destination, DateTime.ParseExact(departureTime, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture));
+                trains[i] = train;
+            }
+            return trains.OrderBy(x => x.TrainNumber).ToArray();
+        }
+
+        private static Train FindTrainByTrainNumber(Train[] trains)
+        {
+            Console.Write("Enter train number to search: ");
+            string trainNumber = Console.ReadLine();
+            return Array.Find(trains, x => x.TrainNumber == trainNumber);
         }
     }
 }
---
+++ ClassWork/Train.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    class Train
+    {
+        public string TrainNumber { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+        public Train(string TrainNumber, string Destination, DateTime DepartureTime)
+        {
+            this.TrainNumber = TrainNumber;
+            this.Destination = Destination;
+            this.DepartureTime = DepartureTime;
+        }
+
+        public void PrintTrainInfo()
+        {
+            Console.WriteLine($"Train Number: {TrainNumber} Destination: {Destination} Departure time: {DepartureTime}");
+        }
+    }
+}
