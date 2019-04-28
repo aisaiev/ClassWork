a01c78bbc65a6c0a08365ba7e114f822bb20a88f
Anton <antonio.de.tores@gmail.com>
2019-04-28 20:16:04 +0300

    Task3 implemented

---
+++ ClassWork/Handler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    class Handler
+    {
+        public static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        public static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+    }
+}
--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -11,40 +11,14 @@ namespace ClassWork
     {
         static void Main(string[] args)
         {
-            Train[] trains = FillTrainsArray(2);
-            Train train = FindTrainByTrainNumber(trains);
-            if (train != null)
-            {
-                train.PrintTrainInfo();
-            }
-            else
-            {
-                Console.WriteLine("Train is not found");
-            }
-        }
-
-        private static Train[] FillTrainsArray(int numberOfTrains)
-        {
-            Train[] trains = new Train[numberOfTrains];
-            for (int i = 0; i < numberOfTrains; i++)
-            {
-                Console.Write("Enter train number: ");
-                string trainNumber = Console.ReadLine();
-                Console.Write("Enter destination: ");
-                string destination = Console.ReadLine();
-                Console.Write("Enter departure time (yyyy-mm-dd hh:mm): ");
-                string departureTime = Console.ReadLine();
-                Train train = new Train(trainNumber, destination, DateTime.ParseExact(departureTime, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture));
-                trains[i] = train;
-            }
-            return trains.OrderBy(x => x.TrainNumber).ToArray();
-        }
-
-        private static Train FindTrainByTrainNumber(Train[] trains)
-        {
-            Console.Write("Enter train number to search: ");
-            string trainNumber = Console.ReadLine();
-            return Array.Find(trains, x => x.TrainNumber == trainNumber);
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            myClass.change = "не изменено";
+            myStruct.change = "не изменено";
+            Handler.ClassTaker(myClass);
+            Handler.StructTaker(myStruct);
+            Console.WriteLine($"myClass.change = {myClass.change}");
+            Console.WriteLine($"myStruct.change = {myStruct.change}");
         }
     }
 }
