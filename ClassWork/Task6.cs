bbb547ced6e99ec124c77effc88431f45d98d450
Anton <antonio.de.tores@gmail.com>
2019-04-28 18:59:37 +0300

    Task6 implemented

---
+++ ClassWork/Task6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    public static class Task6
+    {
+        public static void Longest()
+        {
+            string a = "xyaabbbccccdefww";
+            string b = "xxxxyyyyabklmopq";
+
+            IEnumerable<char> c = a.Distinct();
+            foreach (var item in c)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
