4d2f073b71cd649728c4e6ba1134322c77844886
Anton <antonio.de.tores@gmail.com>
2019-05-11 12:26:49 +0300

    Task1 has been implemented

---
+++ ClassWork/Figures/Figure.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    public class Figure
+    {
+        private int x;
+
+        private int y;
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+        public Figure(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"{this.GetType().Name} was drawn with: x={this.X}, y={this.Y}");
+        }
+    }
+}
---
+++ ClassWork/Figures/Square.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork.Figures
+{
+    internal class Square : Figure
+    {
+        private int side;
+
+        public int Side
+        {
+            get
+            {
+                return side;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    side = value;
+                }
+                else
+                {
+                    side = 1;
+                }
+            }
+        }
+
+        public Square(int x, int y, int side)
+            : base(x, y)
+        {
+            this.Side = side;
+        }
+
+        public new void Draw()
+        {
+            Console.WriteLine($"{this.GetType().Name} was drawn with: x={this.X}, y={this.Y}, side={this.side}");
+        }
+    }
+}
