2e5a8031a674075824bfa56d8fc5f80d0d17c43f
Anton <antonio.de.tores@gmail.com>
2019-06-08 13:10:16 +0300

    Task1 implemented

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace ClassWork
     {
         static void Main(string[] args)
         {
+        }
+
+        static void Task1()
+        {
+            const string PATH = @"C:\Windows\System32";
+            long totalSize = 0;
+            string[] directories = Directory.GetDirectories(PATH);
+            Console.WriteLine($"Amount of directories: {directories.Length}");
+            string[] files = Directory.GetFiles(PATH);
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                totalSize += fileInfo.Length;
+            }
+            Console.WriteLine($"Total size of directories: {totalSize} bytes");
         }
     }
 }
