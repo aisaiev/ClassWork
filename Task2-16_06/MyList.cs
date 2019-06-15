0e528fc746de0077f814b7b15f2083739bdf8706
Anton <antonio.de.tores@gmail.com>
2019-06-15 13:10:13 +0300

    Task2 implemented

---
+++ Task2-16_06/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_16_06
+{
+    class MyList<T> : IMyList<T>
+    {
+        T[] array;
+
+        public int Count { get; private set; }
+
+        public MyList()
+        {
+            this.array = new T[1];
+            this.Count = 0;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.array[index];
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (this.Count == 0)
+            {
+                this.array[0] = item;
+                this.Count++;
+            }
+            else
+            {
+                T[] tempArray = new T[this.Count + 1];
+                array.CopyTo(tempArray, 0);
+                tempArray[array.Length] = item;
+                array = tempArray;
+                this.Count++;
+            }
+        }
+
+        public void Clear()
+        {
+            this.array = new T[1];
+            this.Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            bool contains = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    contains = true;
+                }
+            }
+            return contains;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"MyList Count: {this.Count}")
+                .AppendLine().Append("MyList elements: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                stringBuilder.Append(array[i]).Append(" ");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Task2-16_06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_16_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            Console.WriteLine(myList.Count);
+            Console.WriteLine(myList.ToString());
+        }
+    }
+}
