36b51a0b27963590d8f7a48debb8a97c66cced0e
Anton <antonio.de.tores@gmail.com>
2019-06-15 11:31:58 +0300

    Task1 implemented

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -10,6 +10,25 @@ namespace ClassWork
     {
         static void Main(string[] args)
         {
+            Task1();
+        }
+
+        static void Task1()
+        {
+            DateTime dateTimeNow = DateTime.Now;
+            Console.WriteLine(dateTimeNow.ToString("MM/dd/yyyy"));
+
+            DateTime yesterdayDay = DateTime.Now.AddDays(-1);
+            Console.WriteLine(yesterdayDay.ToString("MM/dd/yyyy"));
+
+            DateTime twoMonthBefore = DateTime.Now.AddMonths(-2);
+            Console.WriteLine(twoMonthBefore.ToString("MM/dd/yyyy"));
+
+            DateTime oneYearBefore = DateTime.Now.AddYears(-1);
+            Console.WriteLine(oneYearBefore.ToString("MM/dd/yyyy"));
+
+            DateTime birthDay = new DateTime(1991, 07, 06);
+            Console.WriteLine(birthDay.ToString("MM/dd/yyyy"));
         }
     }
 }
