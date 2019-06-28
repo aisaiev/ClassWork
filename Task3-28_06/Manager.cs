8544246492797c198938df3c367b5ca2750ae835
Anton <antonio.de.tores@gmail.com>
2019-06-28 13:23:09 +0300

    Task3 implemented

---
+++ Task3-28_06/Manager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_28_06
+{
+    class Manager
+    {
+        //
+        // The first part.
+        //
+        private List<Student> studentsList;
+
+        public Manager()
+        {
+            this.studentsList = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            this.studentsList.Add(student);
+        }
+
+        public void DisplayStudentsCount()
+        {
+            Console.WriteLine($"Count of students: {this.studentsList.Count}");
+        }
+
+        public void RemoveStudentByFirstNameAndSecondName(string firstName, string lastName)
+        {
+            int studentIndex = this.studentsList.FindIndex(x => x.FirstName == firstName && x.LastName == lastName);
+            if (studentIndex != -1)
+            {
+                this.studentsList.RemoveAt(studentIndex);
+                Console.WriteLine($"Student {firstName} {lastName} removed");
+            }
+            else
+            {
+                Console.WriteLine($"Student {firstName} {lastName} is not found");
+            }
+        }
+
+        public void PrintAllStudents()
+        {
+            foreach (var student in studentsList)
+            {
+                Console.WriteLine($"First Name: {student.FirstName} Last Name: {student.LastName} Course Number: {student.CourseNumber}" +
+                    $"Group: {student.Group} Faculty: {student.Faculty}");
+            }
+        }
+
+        public Student FindUserByFirstNameAndSecondName(string firstName, string lastName)
+        {
+            return this.studentsList.Find(x => x.FirstName == firstName && x.LastName == lastName);
+        }
+
+        public void AssignTaskForStudent(Student student, string task)
+        {
+            student.tasks.Enqueue(task);
+            Console.WriteLine($"Task '{task}' was assigned to student {student.FirstName} {student.LastName}");
+        }
+    }
+}
---
+++ Task3-28_06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_28_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.AddStudent(new Student("Vasya", "Pupkin", 1, "test group", "test faculty"));
+            manager.AssignTaskForStudent(manager.FindUserByFirstNameAndSecondName("Vasya", "Pupkin"), "Do Task1");
+            manager.AssignTaskForStudent(manager.FindUserByFirstNameAndSecondName("Vasya", "Pupkin"), "Do Task2");
+
+            var student = manager.FindUserByFirstNameAndSecondName("Vasya", "Pupkin");
+            student.ResolveTask();
+            student.ResolveTask();
+        }
+    }
+}
