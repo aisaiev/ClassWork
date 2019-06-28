964733983f986a589f8122012025dba45e0f7d7c
Anton <antonio.de.tores@gmail.com>
2019-06-28 12:56:20 +0300

    Task2 implemented

---
+++ Task2-28_06/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_28_06
+{
+    public class Manager
+    {
+        private Dictionary<Tuple<string, string>, Student> studentDictionary;
+
+        public Manager()
+        {
+            studentDictionary = new Dictionary<Tuple<string, string>, Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            studentDictionary.Add(Tuple.Create(student.FirstName, student.LastName), student);
+        }
+
+        public Student GetStudentByFirstNameAndLastName(string firstName, string lastName)
+        {
+            studentDictionary.TryGetValue(Tuple.Create(firstName, lastName), out Student student);
+            return student;
+        }
+    }
+}
--- Task2-28_06/Program.cs
+++ Task2-28_06/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassWork
+namespace Task2_28_06
 {
     class Program
     {
