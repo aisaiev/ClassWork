bd460491dd7b0020e868720f39a3559e10607db5
Anton <antonio.de.tores@gmail.com>
2019-07-21 14:45:27 +0300

    Task3 implemented

---
+++ Task3-13_07/Currency.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Task3_13_07
+{
+    class Currency
+    {
+        [JsonProperty("currencyCodeA")]
+        private int CurrencyCodeA { get; set; }
+
+        [JsonProperty("currencyCodeB")]
+        private int CurrencyCodeB { get; set; }
+
+        [JsonProperty("date")]
+        private long Date { get; set; }
+
+        [JsonProperty("rateBuy")]
+        private float RateBuy { get; set; }
+
+        [JsonProperty("rateSell")]
+        private float RateSell { get; set; }
+
+        [JsonProperty("rateCross")]
+        private float RateCross { get; set; }
+
+        public List<Currency> currencies;
+
+        private readonly string URL = "https://api.monobank.ua/bank/currency";
+
+        public async Task GetCurrenciesAsync()
+        {
+            using(HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(URL);
+                if (response.IsSuccessStatusCode)
+                {
+                    string currenciesResponce = await response.Content.ReadAsStringAsync();
+                    this.currencies = JsonConvert.DeserializeObject<List<Currency>>(currenciesResponce);
+                }
+            }
+        }
+    }
+}
---
+++ Task3-13_07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_13_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Currency currency = new Currency();
+            Task.Run(async () => { await currency.GetCurrenciesAsync(); }).Wait();
+        }
+    }
+}
