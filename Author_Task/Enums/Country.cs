2fe11f733a02251cd5c76bf3a56f26067bddd5e5
Anton Isaev <aisaev@intetics.com>
2020-01-06 08:38:06 +0200

    Author_Task 1st and 3rd part implemented

---
+++ Author_Task/Enums/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Author_Task.Enums
+{
+    public enum Country
+    {
+        UKR = 0,
+        USA = 1,
+        GBR = 2,
+        DEU = 3
+    }
+}
