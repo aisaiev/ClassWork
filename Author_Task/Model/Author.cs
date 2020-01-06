48ea5203457f86260c72a1c49e2ec1e2c2eca883
Anton Isaev <aisaev@intetics.com>
2020-01-06 08:29:30 +0200

    Author_Task 1st and 3rd part implemented

---
+++ Author_Task/Model/Author.cs
@@ -0,0 +1,43 @@
+﻿using Author_Task.Enums;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Author_Task.Model
+{
+    public class Author : EntityBase, ICloneable
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Country Country { get; set; }
+
+        public Language Language { get; set; }
+
+        public string PlaceOfBirth { get; set; }
+
+        public ObservableCollection<Book> Books { get; set; }
+
+        public Author()
+        {
+            this.BirthDate = DateTime.Now;
+            this.Books = new ObservableCollection<Book>();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Author_Task/Tools/IsNewToTextConverter.cs
@@ -0,0 +1,48 @@
+﻿using Author_Task.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Author_Task.Tools
+{
+    public class IsNewToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Author)
+            {
+                Author author = value as Author;
+                if (author.IsNew)
+                {
+                    return "New Author";
+                }
+                else
+                {
+                    return "Edit Author";
+                }
+            }
+            else if (value is Book)
+            {
+                Book book = value as Book;
+                if (book.IsNew)
+                {
+                    return "New Book";
+                }
+                else
+                {
+                    return "Edit Book";
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
