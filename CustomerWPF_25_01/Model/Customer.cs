4950877937aede2136de84986ca3b6551e20bdd5
Anton <antonio.de.tores@gmail.com>
2020-01-25 13:07:30 +0200

    CustomerWPF_25_01 implemented

---
+++ CustomerWPF_25_01/Model/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerWPF_25_01.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+        public Customer(string firstName, string lastName, string city)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.City = city;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
---
+++ CustomerWPF_25_01/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CustomerWPF_25_01.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerWPF_25_01.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public Customer SelectedCustomer { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.Customers = new ObservableCollection<Customer>()
+            {
+                new Customer("Steven", "King", "New York"),
+                new Customer("Neena", "Delroy", "Washington"),
+                new Customer("Lex", "Smith", "Chicago"),
+                new Customer("Alexander", "Gates", "Los Angeles"),
+                new Customer("Bruce", "Willis", "Atlanta"),
+            };
+        }
+    }
+}
