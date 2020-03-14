59f554a8488260abc1ef214ac177216d701d8acf
Anton Isaev <aisaev@intetics.com>
2020-03-14 15:56:18 +0200

    added DataSeeding from DBContext

--- Stores.Model/Context/StoresDbContext.cs
+++ Stores.Model/Context/StoresDbContext.cs
@@ -95,13 +95,6 @@ namespace Store.Model.Context
                     .HasColumnName("store_id");
                 entity.Property(entity => entity.StaffId)
                     .HasColumnName("staff_id");
-
-                //
-                // Fixed 'may cause cycles or multiple cascade path'.
-                //
-                entity.HasOne(order => order.Store)
-                    .WithMany(store => store.Orders)
-                    .OnDelete(DeleteBehavior.NoAction);
             });
 
             modelBuilder.Entity<OrderItem>(entity =>
@@ -191,6 +184,75 @@ namespace Store.Model.Context
                 entity.Property(entity => entity.BrandName)
                     .HasColumnName("brand_name");
             });
+
+
+            Brand brand1 = new Brand { BrandId = 1, BrandName = "Electra" };
+            Brand brand2 = new Brand { BrandId = 2, BrandName = "Haro" };
+            Brand brand3 = new Brand { BrandId = 3, BrandName = "Heller" };
+            Brand brand4 = new Brand { BrandId = 4, BrandName = "Pure Cycles" };
+            Brand brand5 = new Brand { BrandId = 5, BrandName = "Ritchey" };
+            Brand[] brands = { brand1, brand2, brand3, brand4, brand5 };
+            modelBuilder.Entity<Brand>().HasData(brands);
+
+            Category category1 = new Category { CategoryId = 1, CategoryName = "Children Bicycles" };
+            Category category2 = new Category { CategoryId = 2, CategoryName = "Comfort Bicycles" };
+            Category category3 = new Category { CategoryId = 3, CategoryName = "Cruisers Bicycles" };
+            Category category4 = new Category { CategoryId = 4, CategoryName = "Cyclocross Bicycles" };
+            Category category5 = new Category { CategoryId = 5, CategoryName = "Electric Bikes" };
+            Category category6 = new Category { CategoryId = 6, CategoryName = "Mountain Bikes" };
+            Category[] categories = { category1, category2, category3, category4, category5, category6 };
+            modelBuilder.Entity<Category>().HasData(categories);
+
+            Product product1 = new Product { ProductId = 1, ProductName = "Electra Townie Original 21D - 2016", BrandId = 1, CategoryId = 3, ModelYear = new DateTime(2016, 1, 1), ListPrice = 550 };
+            Product product2 = new Product { ProductId = 2, ProductName = "Pure Cycles Vine 8-Speed - 2016", BrandId = 4, CategoryId = 3, ModelYear = new DateTime(2016, 1, 1), ListPrice = 430 };
+            Product product3 = new Product { ProductId = 3, ProductName = "Haro Downtown 16 - 2017", BrandId = 2, CategoryId = 1, ModelYear = new DateTime(2017, 1, 1), ListPrice = 330 };
+            Product product4 = new Product { ProductId = 4, ProductName = "Haro Shredder 20 - 2017", BrandId = 2, CategoryId = 1, ModelYear = new DateTime(2017, 1, 1), ListPrice = 210 };
+            Product product5 = new Product { ProductId = 5, ProductName = "Ritchey Timberwolf Frameset - 2016", BrandId = 5, CategoryId = 6, ModelYear = new DateTime(2016, 1, 1), ListPrice = 380 };
+            Product[] products = { product1, product2, product3, product4, product5 };
+            modelBuilder.Entity<Product>().HasData(products);
+
+            Stock stock1 = new Stock { StoreId = 1, ProductId = 1, Quantity = 5 };
+            Stock stock2 = new Stock { StoreId = 1, ProductId = 2, Quantity = 15 };
+            Stock stock3 = new Stock { StoreId = 1, ProductId = 3, Quantity = 20 };
+            Stock stock4 = new Stock { StoreId = 1, ProductId = 4, Quantity = 1 };
+            Stock stock5 = new Stock { StoreId = 1, ProductId = 5, Quantity = 10 };
+            Stock[] stocks = { stock1, stock2, stock3, stock4, stock5 };
+            modelBuilder.Entity<Stock>().HasData(stocks);
+
+            Customer customer1 = new Customer { CustomerId = 1, FirstName = "Debra", LastName = "Burks", Phone = "(516) 583-7761", Email = "debra.burks@yahoo.com", Street = "9273 Thorne Ave.", City = "Orchard Park", State = "NY", ZipCode = "14127" };
+            Customer customer2 = new Customer { CustomerId = 2, FirstName = "Kasha", LastName = "Todd", Phone = "(212) 945-8823", Email = "kasha.todd@yahoo.com", Street = "910 Vine Street", City = "Campbell", State = "CA", ZipCode = "95008" };
+            Customer customer3 = new Customer { CustomerId = 3, FirstName = "Tameka", LastName = "Fisher", Phone = "(562) 215-2907", Email = "tameka.fisher@aol.com", Street = "769C Honey Creek St.", City = "Redondo Beach", State = "CA", ZipCode = "90278" };
+            Customer customer4 = new Customer { CustomerId = 4, FirstName = "Daryl", LastName = "Spence", Phone = "(510) 246-8375", Email = "daryl.spence@aol.com", Street = "988 Pearl Lane", City = "Uniondale", State = "NY", ZipCode = "11553" };
+            Customer customer5 = new Customer { CustomerId = 5, FirstName = "Charolette", LastName = "Rice", Phone = "(916) 381-6003", Email = "charolette.rice@msn.com", Street = "107 River Dr.", City = "Sacramento", State = "CA", ZipCode = "95820" };
+            Customer[] customers = { customer1, customer2, customer3, customer4, customer5 };
+            modelBuilder.Entity<Customer>().HasData(customers);
+
+            OrderItem orderItem1 = new OrderItem { OrderId = 1, ItemId = 1, ProductId = 1, Quantity = 1, ListPrice = 600, Discount = 0.2 };
+            OrderItem orderItem2 = new OrderItem { OrderId = 1, ItemId = 2, ProductId = 3, Quantity = 2, ListPrice = 860, Discount = 0.07 };
+            OrderItem orderItem3 = new OrderItem { OrderId = 2, ItemId = 3, ProductId = 2, Quantity = 1, ListPrice = 330, Discount = 0.2 };
+            OrderItem orderItem4 = new OrderItem { OrderId = 3, ItemId = 4, ProductId = 4, Quantity = 1, ListPrice = 210, Discount = 0.2 };
+            OrderItem orderItem5 = new OrderItem { OrderId = 4, ItemId = 5, ProductId = 5, Quantity = 1, ListPrice = 380, Discount = 0.2 };
+            OrderItem orderItem6 = new OrderItem { OrderId = 5, ItemId = 5, ProductId = 5, Quantity = 1, ListPrice = 380, Discount = 0.2 };
+            OrderItem[] orderItems = { orderItem1, orderItem2, orderItem3, orderItem4, orderItem5, orderItem6 };
+            modelBuilder.Entity<OrderItem>().HasData(orderItems);
+
+            Order order1 = new Order { OrderId = 1, CustomerId = 1, OrderStatus = "Shipped", OrderDate = new DateTime(2016, 1, 1), RequiredDate = new DateTime(2016, 1, 3), ShippedDate = new DateTime(2016, 1, 3), StoreId = 1, StaffId = 1 };
+            Order order2 = new Order { OrderId = 2, CustomerId = 2, OrderStatus = "Shipped", OrderDate = new DateTime(2016, 1, 1), RequiredDate = new DateTime(2016, 1, 4), ShippedDate = new DateTime(2016, 1, 3), StoreId = 1, StaffId = 1 };
+            Order order3 = new Order { OrderId = 3, CustomerId = 3, OrderStatus = "Shipped", OrderDate = new DateTime(2016, 1, 2), RequiredDate = new DateTime(2016, 1, 5), ShippedDate = new DateTime(2016, 1, 3), StoreId = 1, StaffId = 2 };
+            Order order4 = new Order { OrderId = 4, CustomerId = 4, OrderStatus = "Shipped", OrderDate = new DateTime(2016, 1, 3), RequiredDate = new DateTime(2016, 1, 4), ShippedDate = new DateTime(2016, 1, 5), StoreId = 1, StaffId = 2 };
+            Order order5 = new Order { OrderId = 5, CustomerId = 5, OrderStatus = "Shipped", OrderDate = new DateTime(2016, 1, 3), RequiredDate = new DateTime(2016, 1, 5), ShippedDate = new DateTime(2016, 1, 6), StoreId = 1, StaffId = 3 };
+            Order[] orders = { order1, order2, order3, order4, order5 };
+            modelBuilder.Entity<Order>().HasData(orders);
+
+            Staff staff1 = new Staff { StaffId = 1, FirstName = "Fabiola", LastName = "Jackson", Email = "fabiola.jackson@bikes.shop", Phone = "(831) 555-5554", Active = true, StoreId = 1 };
+            Staff staff2 = new Staff { StaffId = 2, FirstName = "Mireya", LastName = "Copeland", Email = "mireya.copeland@bikes.shop", Phone = "(831) 555-5555", Active = true, StoreId = 1, ManagerId = 1 };
+            Staff staff3 = new Staff { StaffId = 3, FirstName = "Genna", LastName = "Serrano", Email = "genna.serrano@bikes.shop", Phone = "(831) 555-5556", Active = true, StoreId = 1, ManagerId = 1 };
+            Staff[] staffs = { staff1, staff2, staff3 };
+            modelBuilder.Entity<Staff>().HasData(staffs);
+
+            Stores.Model.Entity.Sales.Store store = new Stores.Model.Entity.Sales.Store() { StoreId = 1, StoreName = "Santa Cruz Bikes", Phone = "(831) 476-4321", Email = "santacruz@bikes.shop", Street = "3700 Portola Drive", City = "Santa Cruz", State = "CA", ZipCode = "95060" };
+            Stores.Model.Entity.Sales.Store[] stores = { store };
+            modelBuilder.Entity<Stores.Model.Entity.Sales.Store>().HasData(stores);
         }
     }
 }
---
+++ Stores.Model/Migrations/20200314135454_LoadDataToDb.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stores.Model.Migrations
+{
+    public partial class LoadDataToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_stores_store_id",
+                table: "orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "store_id",
+                table: "orders",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "brands",
+                columns: new[] { "brand_id", "brand_name" },
+                values: new object[,]
+                {
+                    { 1, "Electra" },
+                    { 2, "Haro" },
+                    { 3, "Heller" },
+                    { 4, "Pure Cycles" },
+                    { 5, "Ritchey" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "category_id", "category_name" },
+                values: new object[,]
+                {
+                    { 6, "Mountain Bikes" },
+                    { 5, "Electric Bikes" },
+                    { 4, "Cyclocross Bicycles" },
+                    { 2, "Comfort Bicycles" },
+                    { 1, "Children Bicycles" },
+                    { 3, "Cruisers Bicycles" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "customers",
+                columns: new[] { "customer_id", "city", "email", "first_name", "last_name", "phone", "state", "street", "zip_code" },
+                values: new object[,]
+                {
+                    { 5, "Sacramento", "charolette.rice@msn.com", "Charolette", "Rice", "(916) 381-6003", "CA", "107 River Dr.", "95820" },
+                    { 1, "Orchard Park", "debra.burks@yahoo.com", "Debra", "Burks", "(516) 583-7761", "NY", "9273 Thorne Ave.", "14127" },
+                    { 2, "Campbell", "kasha.todd@yahoo.com", "Kasha", "Todd", "(212) 945-8823", "CA", "910 Vine Street", "95008" },
+                    { 3, "Redondo Beach", "tameka.fisher@aol.com", "Tameka", "Fisher", "(562) 215-2907", "CA", "769C Honey Creek St.", "90278" },
+                    { 4, "Uniondale", "daryl.spence@aol.com", "Daryl", "Spence", "(510) 246-8375", "NY", "988 Pearl Lane", "11553" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "stores",
+                columns: new[] { "store_id", "city", "email", "phone", "state", "store_name", "street", "zip_code" },
+                values: new object[] { 1, "Santa Cruz", "santacruz@bikes.shop", "(831) 476-4321", "CA", "Santa Cruz Bikes", "3700 Portola Drive", "95060" });
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "product_id", "brand_id", "category_id", "list_price", "model_year", "product_name" },
+                values: new object[,]
+                {
+                    { 3, 2, 1, 330m, new DateTime(2017, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Haro Downtown 16 - 2017" },
+                    { 4, 2, 1, 210m, new DateTime(2017, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Haro Shredder 20 - 2017" },
+                    { 1, 1, 3, 550m, new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Electra Townie Original 21D - 2016" },
+                    { 2, 4, 3, 430m, new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Pure Cycles Vine 8-Speed - 2016" },
+                    { 5, 5, 6, 380m, new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ritchey Timberwolf Frameset - 2016" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "staffs",
+                columns: new[] { "staff_id", "active", "email", "first_name", "last_name", "manager_id", "MangerStaffId", "phone", "store_id" },
+                values: new object[,]
+                {
+                    { 1, true, "fabiola.jackson@bikes.shop", "Fabiola", "Jackson", 0, null, "(831) 555-5554", 1 },
+                    { 2, true, "mireya.copeland@bikes.shop", "Mireya", "Copeland", 1, null, "(831) 555-5555", 1 },
+                    { 3, true, "genna.serrano@bikes.shop", "Genna", "Serrano", 1, null, "(831) 555-5556", 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "orders",
+                columns: new[] { "order_id", "customer_id", "order_date", "order_status", "required_date", "shipped_date", "staff_id", "store_id" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Shipped", new DateTime(2016, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2016, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1 },
+                    { 2, 2, new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Shipped", new DateTime(2016, 1, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2016, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1 },
+                    { 3, 3, new DateTime(2016, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Shipped", new DateTime(2016, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2016, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 1 },
+                    { 4, 4, new DateTime(2016, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Shipped", new DateTime(2016, 1, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2016, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 1 },
+                    { 5, 5, new DateTime(2016, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Shipped", new DateTime(2016, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2016, 1, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "stocks",
+                columns: new[] { "store_id", "product_id", "quantity" },
+                values: new object[,]
+                {
+                    { 1, 3, 20 },
+                    { 1, 4, 1 },
+                    { 1, 1, 5 },
+                    { 1, 2, 15 },
+                    { 1, 5, 10 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "order_itmes",
+                columns: new[] { "order_id", "item_id", "discount", "list_price", "product_id", "quantity" },
+                values: new object[,]
+                {
+                    { 1, 1, 0.20000000000000001, 600m, 1, 1 },
+                    { 1, 2, 0.070000000000000007, 860m, 3, 2 },
+                    { 2, 3, 0.20000000000000001, 330m, 2, 1 },
+                    { 3, 4, 0.20000000000000001, 210m, 4, 1 },
+                    { 4, 5, 0.20000000000000001, 380m, 5, 1 },
+                    { 5, 5, 0.20000000000000001, 380m, 5, 1 }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_stores_store_id",
+                table: "orders",
+                column: "store_id",
+                principalTable: "stores",
+                principalColumn: "store_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_stores_store_id",
+                table: "orders");
+
+            migrationBuilder.DeleteData(
+                table: "brands",
+                keyColumn: "brand_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "category_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "category_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "category_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "order_itmes",
+                keyColumns: new[] { "order_id", "item_id" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "order_itmes",
+                keyColumns: new[] { "order_id", "item_id" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "order_itmes",
+                keyColumns: new[] { "order_id", "item_id" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "order_itmes",
+                keyColumns: new[] { "order_id", "item_id" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "order_itmes",
+                keyColumns: new[] { "order_id", "item_id" },
+                keyValues: new object[] { 4, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "order_itmes",
+                keyColumns: new[] { "order_id", "item_id" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "stocks",
+                keyColumns: new[] { "store_id", "product_id" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "stocks",
+                keyColumns: new[] { "store_id", "product_id" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "stocks",
+                keyColumns: new[] { "store_id", "product_id" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "stocks",
+                keyColumns: new[] { "store_id", "product_id" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "stocks",
+                keyColumns: new[] { "store_id", "product_id" },
+                keyValues: new object[] { 1, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "orders",
+                keyColumn: "order_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "orders",
+                keyColumn: "order_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "orders",
+                keyColumn: "order_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "orders",
+                keyColumn: "order_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "orders",
+                keyColumn: "order_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "brands",
+                keyColumn: "brand_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "brands",
+                keyColumn: "brand_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "brands",
+                keyColumn: "brand_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "brands",
+                keyColumn: "brand_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "category_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "category_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "category_id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "customers",
+                keyColumn: "customer_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "customers",
+                keyColumn: "customer_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "customers",
+                keyColumn: "customer_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "customers",
+                keyColumn: "customer_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "customers",
+                keyColumn: "customer_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "staffs",
+                keyColumn: "staff_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "staffs",
+                keyColumn: "staff_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "staffs",
+                keyColumn: "staff_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "stores",
+                keyColumn: "store_id",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "store_id",
+                table: "orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_stores_store_id",
+                table: "orders",
+                column: "store_id",
+                principalTable: "stores",
+                principalColumn: "store_id");
+        }
+    }
+}
